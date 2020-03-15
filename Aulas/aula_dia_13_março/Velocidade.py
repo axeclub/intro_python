50732c31f7477a56517e5e52d2544829c782ad50
axeclub <62065302+axeclub@users.noreply.github.com>
2020-03-15 13:53:39 -0300

    Add files via upload

---
+++ Aulas/aula_dia_13_março/Velocidade.py
@@ -0,0 +1,12 @@
+import math
+
+t = (43*60)+30 #Tempo em segundos
+s = 10/1.61 #Milhas
+
+
+v = (s/t)*(60*60)
+tm = s/v
+
+
+print('A velocidade é: V =', v, 'milhas/h')
+print('O tempo média é: Tm =', tm, 'h')
